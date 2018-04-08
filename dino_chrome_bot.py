85b554e2646196a3221d1bef2f6aaab93f625e72
Felipe Serra <felipeserra@hotmail.com>
2018-04-08 10:16:29 -0300

    Update dino_chrome_bot.py
    Alterado para trabalhar com resolução 1366X768 padrão nos notebook

--- dino_chrome_bot.py
+++ dino_chrome_bot.py
@@ -1,49 +1,51 @@
-#!/usr/bin/python
-
-'''
-Dino Chrome Game Bot - Guilherme Junqueira
-
-Python Basico Solyd
-https://solyd.com.br/treinamentos
-'''
-
 import time
 
 from PIL import ImageGrab
 import pyautogui
 
-
-# Region of detections
-# Coordenates for resolution 1600x900
-X = 655.0  # X2 = X + 15
-Y1 = 215
-Y2 = 250
-
-
-# Take screenshot using PIL lib
+X = 243.0  # X2 = X + 15
+Y1 = 310
+Y2 = 350
+N = 0
+N1 = 0.1
 def capture_screen():
     screen = ImageGrab.grab()
     return screen
 
-
-# Detects enemy by diff in pixel color in region of detections
 def detect_enemy(screen):
     aux_color = screen.getpixel((int(X), Y1))
     for x in range(int(X), int(X+15)):
         for y in range(Y1, Y2):
             color = screen.getpixel((x, y))
+            #print(color, time.clock())
             if color != aux_color:
                 return True # Return True for a detected enemy
             else:
                 aux_color = color
 
 
+def detect_update(screen):
+    aux_color = screen.getpixel((int(X), Y1))
+    for x in range(int(X), int(X+15)):
+        for y in range(Y1, Y2):
+            color = screen.getpixel((x, y))
+            #print(color, time.clock())
+            if color != aux_color:
+                return True # Return True for a detected enemy
+            else:
+                aux_color = color                
+
+
 # Dino Jumps
 def jump():
     global X
+    global N
+    N +=1
+    print(N)
     pyautogui.press("up")
-    X += 0.4  # Increment in detection region for increase speed of game
-
+    if (N == 7):
+        X += (1.1+N1)  # Increment in detection region for increase speed of game
+        N = 0
 
 print("Start in 3 seconds...")
 time.sleep(3)
@@ -53,4 +55,3 @@ while True:
     screen = capture_screen()
     if detect_enemy(screen):
         jump()
-
